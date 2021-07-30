41bfb3791414f7fbacfb07f3a699fb825c14fbd1
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-30 03:59:24 +0000

    [add]4-10復習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def index
     @user=current_user
     @books=Book.all
-    @book=Book.new
+    @booknew=Book.new
   end
 
   def create
@@ -15,13 +15,31 @@ class BooksController < ApplicationController
 
   def show
     @book=Book.find(params[:id])
+    @user=@book.user
+    @booknew=Book.new
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+   book=Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
-private
+  private
 
-def book_params
-  params.require(:book).permit(:title,:body)
-end
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   
   def index
+    @users=User.all
+    @user=current_user
+    @booknew=Book.new
   end
   
   def show
+    @user=User.find(params[:id])
+    @books=@user.books
+    @booknew=Book.new
   end
   
 end
