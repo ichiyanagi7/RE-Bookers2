df370421408d45a1c2b42e9ee4d6154e9ea98527
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-07-30 08:01:51 +0000

    [finish]4-10復習

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,31 @@ class UsersController < ApplicationController
   def index
     @users=User.all
     @user=current_user
-    @booknew=Book.new
+    @book=Book.new
   end
 
   def show
     @user=User.find(params[:id])
     @books=@user.books
-    @booknew=Book.new
+    @book=Book.new
   end
 
   def edit
     @user=User.find(params[:id])
+    if @user ==current_user
+      render:edit
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
-    user=User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user)
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user),notice:"You have updated user successfully."
+    else
+      render:edit
+    end
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07f69f911716301209460646410ee443c0c2af926dce27f3459f475a3374ae2e969504158bcbee25e9adedd10b4b292f05827f2504cc78fb35e08ca69e4100b3'
+
