6d5d837d528f0d211941801ff2dde84e23d02325
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-08-02 08:01:27 +0000

    [start]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,27 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
 
+  has_many :follower,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+  has_many :followed,class_name:"Relationship",foreign_key:"followed_id",dependent: :destroy
+  has_many :follower_user,through: :follower,source: :followed
+  has_many :followed_user,through: :followed,source: :follower
+
+  # ユーザーフォロー
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーフォロー解除
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+
+
   attachment :profile_image
 
   validates :name,length:{minimum:2,maximum:20},uniqueness:true
