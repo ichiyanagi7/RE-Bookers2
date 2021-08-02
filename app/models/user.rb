00abc150fa9cccf3d413aa833f02bced5b0760a4
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-08-02 12:56:38 +0000

    [fix]応用課題4 エラー修正中

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
 
+# フォロー取得
   has_many :follower,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+# フォロワー取得
   has_many :followed,class_name:"Relationship",foreign_key:"followed_id",dependent: :destroy
+# 自分をフォローしている人
   has_many :follower_user,through: :follower,source: :followed
-  has_many :followed_user,through: :followed,source: :follower
+# 自分がフォローしている人
+  has_many :following_user,through: :followed,source: :follower
 
   # ユーザーフォロー
   def follow(user_id)
