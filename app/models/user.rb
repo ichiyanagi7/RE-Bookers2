6cbf323ebcf6f571816274c30e0b37c72ba208fc
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-08-02 13:21:39 +0000

    [add]応用課題4 アソシエーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,11 @@ class User < ApplicationRecord
   has_many :follower,class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
 # フォロワー取得
   has_many :followed,class_name:"Relationship",foreign_key:"followed_id",dependent: :destroy
-# 自分をフォローしている人
-  has_many :follower_user,through: :follower,source: :followed
+  
 # 自分がフォローしている人
-  has_many :following_user,through: :followed,source: :follower
+  has_many :following_user,through: :follower,source: :followed
+# 自分をフォローしている人
+  has_many :follower_user,through: :followed,source: :follower
 
   # ユーザーフォロー
   def follow(user_id)
