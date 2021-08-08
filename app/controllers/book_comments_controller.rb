1378b770a0c9a4f9f272ae5fb9f01c38f8bb5edb
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-08-08 11:24:24 +0000

    [finish]応用課題6 コメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,17 +2,17 @@ class BookCommentsController < ApplicationController
 
   def create
     @book=Book.find(params[:book_id])
-    comment=current_user.book_comments.new(book_comment_params)
-    comment.book_id=@book.id
-    comment.save
-    redirect_to request.referer
+    @comment=current_user.book_comments.new(book_comment_params)
+    @comment.book_id=@book.id
+    @comment.save
+    # redirect_to request.referer
   end
 
   def destroy
     @book=Book.find(params[:book_id])
-    comment=BookComment.find_by(id: params[:id],book_id: params[:book_id])
-    comment.destroy
-    redirect_to request.referer
+    @comment=BookComment.find_by(id: params[:id],book_id: params[:book_id])
+    @comment.destroy
+    # redirect_to request.referer
   end
 
   private
