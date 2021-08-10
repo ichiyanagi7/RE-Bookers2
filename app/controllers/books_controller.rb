0229afed6e47d86c5357fa22d487670f412f2b7e
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-08-10 10:32:35 +0000

    [start]応用課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,12 @@ class BooksController < ApplicationController
   def index
     @user=current_user
     @books=Book.all
+    @book_ranks=Book.find(Favorite.group(:book_id).order("count(book_id) desc").limit(10).pluck(:book_id))
+        # Favorite.group(:book_id) :Bookの番号(book_id)が同じものにグループを分ける（1つの本にたくさんのfavoriteがある状態）
+        # order("count(book_id) desc") ：それを番号の多い順に並び替える
+        # limit(10) :表示する最大数を決める。ここでは10
+        # pluck(:book_id) : :book_idカラムのみを数字で取り出すように指定
+        # Book.find() :最終的にpluckで取り出す数字をBookのIDとすることでいいね順にBookを取り出す
     @book=Book.new
   end
 
